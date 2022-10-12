fb877b2cd12c446ea8c4717fdd6cec08b2ea3f2b
Static132 <Static.132@yahoo.com>
2022-10-12 14:00:46 +0100

    Big Changes to bullets and updates
    introduced meta_update(), powerup variable, scouts move faster than more powerful ships, space to start menu,

--- System.py
+++ System.py
@@ -7,6 +7,7 @@ WIDTH = 1500
 HEIGHT = 900
 mainscreen = pygame.display.set_mode((WIDTH,HEIGHT))
 title_text = pygame.font.SysFont("systemtextfont", 100)
+main_text = pygame.font.SysFont("systemtextfont", 20)
 #sets the width and height of the game
 
 pygame.display.set_caption("SYSTEM")
@@ -52,6 +53,7 @@ playerbullets = []
 playermaxbullets = 3
 playerbulletwidth = 10
 playerbulletheight= 10
+powerup = 0
 
 enemybullets = []
 enemybulletwidth = 5
@@ -80,6 +82,12 @@ def loadlevel(level_list):
 #example spawnpoint = [scout,1],[x,y]
 
 #----------level renderer, repeat once level has been loaded----------------
+def meta_update():
+    pygame.draw.rect(mainscreen,(10,10,10),(0,700,1000,200)) #health/shields bar
+    pygame.draw.rect(mainscreen,(255,50,0),(50,750,playerhealth*9,100)) #health
+    pygame.draw.rect(mainscreen,(0,205,255),(0,700,333*len(playerbullets),50)) #bullet "charge"
+    pygame.display.update(0,700,1000,200)
+
 def levelrender():
     global level
     global current_level
@@ -109,11 +117,11 @@ def levelrender():
                     enemybullets.append([(enemy[1])[0],(enemy[1])[1]+sprite.height,(enemy[0])[0]])
 
         #----enemy slowly move toward player----
-        if randint(0,3*len(current_level)^2) < (enemy[0])[0]:
-            (enemy[1])[1] += 3
+        if randint(0,len(current_level)^2) < (enemy[0])[0]:
+            (enemy[1])[1] += 4 - (enemy[0])[0]
             if (enemy[1])[1] > 700:
                 (enemy[1])[1] -= 750
-        
+
         #----enemy upgrades----
         if randint(0,500*len(current_level)^2) < (enemy[0])[0]:
             (enemy[0])[0] += 1
@@ -130,12 +138,14 @@ def levelrender():
                             if (enemy[0])[1] == 0:
                                 current_level.remove(enemy)
                             playerbullets.remove(bullet)
+                            meta_update()
 
     for bullet in playerbullets:
         pygame.draw.rect(mainscreen,(255,255,255),(bullet[0]-playerbulletwidth/2,bullet[1],playerbulletwidth,playerbulletheight))
         bullet[1] -= 5
         if bullet[1] < 0 - playerbulletheight:
             playerbullets.remove(bullet)
+            meta_update()
     
 #-----enemy bullet rendering-----
 
@@ -165,9 +175,7 @@ def levelrender():
                 if bullet[0] < player.x + player.width:
                     if bullet[1] < player.y + player.height:
                         if bullet[1] > player.y:
-                            pygame.draw.rect(mainscreen,(10,10,10),(0,700,1000,200)) #health/shields bar
-                            pygame.draw.rect(mainscreen,(255,50,0),(50,750,playerhealth*9,100))
-                            pygame.display.update(0,700,1000,200)
+                            meta_update()
                             playerhealth -= bullet[2]
                             enemybullets.remove(bullet)
 
@@ -190,6 +198,9 @@ def firstload():
     pygame.draw.rect(mainscreen,(255,50,0),(50,750,playerhealth*9,100))
     pygame.draw.rect(mainscreen,(15,15,15),(1000,0,500,900)) #metadata bar
     mainscreen.blit(title_text.render('SYSTEM', False, (255, 255, 255)),(1010,10,)) # title text
+    mainscreen.blit(main_text.render('Press space to start', False, (255, 255, 255)),(1010,100,)) # title text
+
+
     pygame.display.update()
     current_level = loadlevel([
         [1,1],[2,1],[1,1],[1,1],[1,1],[1,1],
@@ -201,6 +212,18 @@ firstload()
 
 #---------------------LIVE INPUT, will probably be run at the end of the code-----------------------
 def main():
+
+    live = True
+    while live:
+        #press space to start
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                    
+                if event.key == pygame.K_ESCAPE:
+                    pygame.quit
+                if event.key == pygame.K_SPACE:
+                    live = False
+
     live = True
     while live:
         clock.tick(60)
@@ -222,7 +245,8 @@ def main():
                     player.movex += 1
                 if event.key == pygame.K_w:
                     if len(playerbullets) < playermaxbullets:
-                        playerbullets.append([player.x + player.width//2, player.y])
+                        playerbullets.append([player.x + player.width//2, player.y,powerup])
+                        meta_update()
 
             if event.type == pygame.KEYUP:  #records key unpresses
                 if event.key == pygame.K_q:
